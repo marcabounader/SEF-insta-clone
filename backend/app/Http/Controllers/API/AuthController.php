347bd048967ad55ab39732cc0358a796d83ee9b7
marcabounader92 <marcabounader92@outlook.com>
2023-08-08 22:47:12 +0300

    Server: added username to validation

--- backend/app/Http/Controllers/API/AuthController.php
+++ backend/app/Http/Controllers/API/AuthController.php
@@ -45,6 +45,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
+            'username' => 'required|string|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
 
