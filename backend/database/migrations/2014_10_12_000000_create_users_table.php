b1292c4c83ec1c065e1ce5394403a284d9d600ac
marcabounader92 <marcabounader92@outlook.com>
2023-08-08 22:40:27 +0300

    Server: added migration

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,34 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::create('posts', function (Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->mediumText('image_url');
+            $table->integer('likes')->default(0);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+        Schema::create('followings', function (Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('follower_id');
+            $table->unsignedBigInteger('following_id');
+            $table->foreign('follower_id')->references('id')->on('users');
+            $table->foreign('following_id')->references('id')->on('users');
+        });
+        Schema::create('likes', function (Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('follower_id');
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('follower_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts');
         });
     }
 
