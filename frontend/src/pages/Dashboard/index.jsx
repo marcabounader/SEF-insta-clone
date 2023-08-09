377cb3d8cff5215780c55e88119e65b5afab7ed9
marcabounader92 <marcabounader92@outlook.com>
2023-08-09 18:33:21 +0300

    Client: added myposts, following posts, likes

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -2,15 +2,31 @@ import { useState } from "react";
 import NavBar from "../../components/NavBar";
 import './dashboard.css'
 import axios from "axios";
+import PostList from "../../components/PostList";
+import AddPost from "../../components/AddPost";
 const Dashboard = () => {
     const [isSearchOpen, setIsSearchOpen] = useState(false);
     const [search,setSearch]=useState("");
     const [users,setUsers]=useState([]);
+    const [posts,setPosts]=useState([]);
+    const [myPosts,setMyPosts]=useState(false);
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false)
+    const handleOpenAddModal = () => setIsAddModalOpen(true)
+    const handleCloseAddModal = () => setIsAddModalOpen(false)
+
     const handleSearch = () =>{
         if(isSearchOpen){
             setIsSearchOpen(false)
         } else{
             setIsSearchOpen(true)
+        }
+    }
+
+    const handleMyPosts = (myPost) =>{
+        if(myPost){
+            setMyPosts(true)
+        } else{
+            setMyPosts(false)
         }
     }
 
@@ -33,34 +49,49 @@ const Dashboard = () => {
     }
 
     const handleFollowSubmit = async (e)=>{
-        const $following_id=e.target.id;
+        const following_id=e.target.id;
         try{
-            const response=await axios.post(`http://localhost:8000/api/follow/`,{following_id:$following_id},config);
+            const response=await axios.post(`http://localhost:8000/api/follow/`,{following_id:following_id},config);
             if(response.data['status']=="success"){
-                console.log('marc');
+                console.log(response.data);
             }
         }catch(e){
             console.log(e);
         }
     };
+
+    const handlelike = async (post_id,following_id) => {
+        try{
+            const response=await axios.post(`http://localhost:8000/api/like-post/`,{post_id:post_id,following_id:following_id},config);
+            if(response.data['status']=="success"){
+                console.log(response.data);
+            }
+        }catch(e){
+            console.log(e);
+        }
+    }
     return ( 
-        <div className="dashboard-container flex-column">
+        <div className="dashboard-container flex-row">
             <div className="side-container flex-row start">
-                <NavBar className='side-nav' handleOpenSearch={handleSearch}/>
+                <NavBar className='side-nav' handleOpenSearch={handleSearch} handleMyPosts={handleMyPosts} handleOpenAddModal={handleOpenAddModal}/>
                 {
                     isSearchOpen && <div className="search-bar flex-col align-center">
 
                         <input type="text" id="search" placeholder="Username or Name" onKeyUp={handleSearchSubmit}/>
-                        <div className="users">
+                        <div className="users flex-col between">
                             {users.map((user)=>(
                                 <div className="user-card" key={user.id}>
-                                    <h4 className="flex-row around">{user.name} </h4>
-                                    <div className="flex-row around">{user.username}<i id ={user.id} className="fa-solid fa-plus" onClick={handleFollowSubmit}></i></div>
+                                    <div className="flex-row">{user.name} </div>
+                                    <div className="flex-row between">{user.username}<i id ={user.id} className="fa-solid fa-plus" onClick={handleFollowSubmit}></i></div>
                                 </div>
                             ))}
                         </div>
                     </div>
                 }
+            </div>
+            <AddPost isOpen={isAddModalOpen} handleCloseAddModal={handleCloseAddModal}></AddPost>
+            <div className="post-container flex-col">
+                <PostList setPosts={setPosts} posts={posts} handlelike={handlelike} myPosts={myPosts}/>
             </div>
         </div>
 
