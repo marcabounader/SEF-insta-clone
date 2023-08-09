3aaa36175bc838f08a24650ab969e0c71c827949
marcabounader92 <marcabounader92@outlook.com>
2023-08-10 00:32:14 +0300

    Client: updated nav bar

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,32 +1,45 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { button } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../instagram-logo.svg'
 import './nav.css'
 
 // import { ReactComponent as Brand } from '../../assets/icons/logo.svg'
 
-const NavBar = ({handleOpenSearch, handleMyPosts,handleOpenAddModal}) => {
+const NavBar = ({handleOpenSearch, handleMyPosts,handleOpenAddModal,logout}) => {
   return (
-    <nav className="nav-container">
-      <div>
-        <ul>
-          <li>
+    <nav className="nav-container flex-col">
           <Logo className="insta-logo"/>
-          </li>
-          <li>
-          <NavLink onClick={()=>{handleMyPosts(false)}}>Home</NavLink>
-          </li>
-          <li>
-          <NavLink onClick={handleOpenSearch}>Search</NavLink>
-          </li>
-          <li>
-          <NavLink onClick={()=>{handleMyPosts(true)}}>My Posts</NavLink>
-          </li>
-          <li>
-          <NavLink onClick={handleOpenAddModal}>Add Post</NavLink>
-          </li>
-        </ul>
-      </div>
+          <div className='nav-subcontainer flex-col between'>
+            <button onClick={()=>{handleMyPosts(false)}}>
+                <span>
+                  <i class="fa-solid fa-house"></i>
+                  <span>Home</span>
+                </span>
+              </button>
+
+              <button onClick={handleOpenSearch}>
+                <span>
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <span>Search</span>
+                  </span>
+              </button>
+            <button onClick={handleOpenAddModal}>
+              <span>
+                <i class="fa-solid fa-square-plus"></i>
+                <span>Add Post</span>
+              </span>
+            </button>
+
+            <button onClick={()=>{handleMyPosts(true)}}>
+              <span>
+                <i class="fa-solid fa-user"></i>
+                <span>Profile</span>
+              </span>
+            </button>
+            <i className="fa-solid fa-right-from-bracket" onClick={logout}></i>
+
+          </div>
+
     </nav>
   )
 }
