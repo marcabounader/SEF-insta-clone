1d8c6ea3601c538cba41f9e5467d3dc4d7013d53
marcabounader92 <marcabounader92@outlook.com>
2023-08-10 12:19:22 +0300

    Server: added post model user function

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Post extends Model
 {
@@ -12,4 +15,9 @@ class Post extends Model
     protected $fillable = [
         'likes'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
